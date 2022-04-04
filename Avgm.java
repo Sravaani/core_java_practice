01449430dd23a438691bf4e073ef1fb94748fea9
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-04 18:44:43 +0530

    Add files via upload

---
+++ Avgm.java
@@ -0,0 +1,15 @@
+// main class
+class Avgm
+  {
+    // main method
+    public static void main(String[] args)
+    {
+      // intializing variables
+      int t=60, s=55, m=65, sum;
+    //  adding the total marks  
+      sum=t+s+m;
+      // printing result
+      System.out.println("Total marks in 3 subjects is "+ (sum));
+      System.out.println("average of marks is " +(sum/3));
+    }
+  }
