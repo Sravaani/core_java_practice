cc574ee6090a3e6b3a757e954d13b9f876ee1563
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-04 18:47:54 +0530

    Add files via upload

---
+++ Threen.java
@@ -0,0 +1,27 @@
+// main class
+class Threen
+  {
+    // main method
+    public static void main(String[] args)
+    {
+      // intializing varibles
+      int a=3;
+      int b=14;
+      int c=6;
+      // if condition
+      if (a>b&&a>c)
+      {
+        // printing
+       System.out.println("a is graeter number");
+      }
+     else if (b>a&&b>c)
+      {
+        System.out.println("b is graeter number");
+      }
+      else
+     {
+       System.out.println("c is graeter");
+     }
+    }
+    
+  }
