637495ed74ce1306cedf2c3849ca4f380cd6d80e
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-04 18:48:36 +0530

    Add files via upload

---
+++ Ifc.java
@@ -0,0 +1,18 @@
+class Ifc{
+  // main method
+  public static void main (String[] args)
+  {
+    // intialize variables
+    int runs=105;
+    // if condition starts and exceute only if its true
+    if(runs >= 100)
+    {
+      //printing the statements if its true
+      System.out.println("Yes he can win the match");
+      }
+    if(runs <= 100)
+    {
+      System.out.println("He wont win the match");
+    }
+    }
+  }
