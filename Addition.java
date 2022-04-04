8540107d100026303babac68223397106570c139
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-04 18:37:18 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+class Addition
+  // main class
+  {
+    // main method
+    public static void main(String[] args)
+    {
+      // printing statement to enter the numbers
+      System.out.println("enter two numbers");
+      // intializing variables with assigned value
+      int a=11;
+      int b=13;
+      System.out.println("a=11");
+      System.out.println("b=13");
+      // perform sum operation
+      int sum=a+b;
+      // print the output
+      System.out.println("sum is:" +sum);
+    }
+  }
