8b09991d95abc88355ec267e3f98b25c87eef8cb
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-04 18:42:22 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,20 @@
+// main class
+class Age
+  {
+    // main method
+    public static void main(String[] args)
+    {
+      // intialize variables
+      int age=16;
+      // if condition checking given condition is true or not
+     if(age>=18)
+     {
+       // printing the result
+      System.out.println("Eligible for vote"); 
+     }
+      else
+     {
+      System.out.println("Not Eligible for vote"); 
+     }
+    }
+  }
