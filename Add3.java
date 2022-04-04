397215491f7858e40aef2bd9ab489a894ae5cb0c
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-04 18:40:06 +0530

    Add files via upload

---
+++ Add3.java
@@ -0,0 +1,17 @@
+class Add3 {
+ // main class
+public static void main(String[] args)
+  // main method
+  {
+//intializing variables a with 10, b with 20, c with 30;
+int a=10, b=20, c=30;
+    // intializing sum product and sub variables
+ // int sum, product, sub;
+  //printing summ of 3 numbers
+System.out.println("sum of a, b, c is " +(a+b+c));
+// product of 3 numbers
+System.out.println("product of a, b, c is " +(a*b*c));
+// sub of 3 numbers
+  System.out.println("subtract of a, b, c is " +(a-b-c));
+    }
+}
