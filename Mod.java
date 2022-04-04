510fa8310b7b2ef79e628e72b079af1b6ae2a127
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-04 19:04:45 +0530

    Add files via upload

---
+++ Mod.java
@@ -0,0 +1,21 @@
+// main class
+class Mod{
+  // main method
+  public static void main(String[] args){
+
+    // intialize variable
+    int n=9;
+    // if condition if ts true execute if block
+    if(n/2==0)
+    {
+      // printing statement if its true
+      System.out.println("given number is even");
+    }
+    else{
+      // printing statement if its false
+      System.out.println("given number is odd");
+    }
+  }
+}
+
+  
