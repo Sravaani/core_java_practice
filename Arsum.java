40194dbbc3d2cbf9730697a22b63777dfdae5a7e
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-05 17:37:28 +0530

    Add files via upload

---
+++ Arsum.java
@@ -0,0 +1,12 @@
+class Arsum
+  {
+    public static void main(String[] args){
+      int a[]={12,90,34,56,33};
+      int sum=0;
+      for(int i=0;i<a.length;i++)
+      {
+        sum=sum+a[i];
+      }
+        System.out.println("sum of arrays: " +sum);
+    }
+  }
