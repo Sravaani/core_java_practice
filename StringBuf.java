fcd712f99fcd2d4264e9c87fcb904012451ff56b
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-06 16:15:07 +0530

    Add files via upload

---
+++ StringBuf.java
@@ -0,0 +1,13 @@
+class StringBuf{
+
+  public static void main(String args[]){
+
+String str=" hi hello ";
+
+    System.out.println(str.length());
+    str=str.trim();
+    System.out.println(str.length());
+    
+    
+  }
+}
