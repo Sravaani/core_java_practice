ebd45bc441afc14b2d0000cd1b60c4d9bf53f882
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-06 16:14:14 +0530

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,14 @@
+class Example2
+{
+ public static void main(String args[]){
+
+ String str="Hi hello bitLabs";
+//System.out.println(str.apend("Sravani"));
+ System.out.println(str.toUpperCase());
+System.out.println(str.toLowerCase());
+System.out.println(str.substring(5));
+System.out.println(str.substring(4,9)); // 4-8
+
+ System.out.println(str.charAt(5));
+}
+}
