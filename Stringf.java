de310fab14acb8a25c116ab21c1b9b02a856c1c1
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-06 16:16:09 +0530

    Add files via upload

---
+++ Stringf.java
@@ -0,0 +1,27 @@
+class Stringf{
+
+ public static void main(String args[]){
+
+ String str="welcome to bitLabs hi welcome hi";
+
+ String words[]=str.split(" ");
+int count=1;
+for(int i=0;i<words.length;i++){
+
+ for(int j=i+1;j<words.length;j++){
+if(words[i].equals(words[j])){
+count++;
+words[j]=" ";
+}
+}
+if(!words[i].equals(" ")){
+System.out.println(words[i]+" : "+count);
+}
+count=1;
+}
+}
+}
+
+
+
+
