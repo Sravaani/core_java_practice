7732ee8b0b475aeace1da6cb434c3b296cecc09a
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-09 18:56:00 +0530

    Add files via upload

---
+++ Ifc1 (1).java
@@ -0,0 +1,16 @@
+// main class
+class Ifc1{ 
+  // main method
+  public static void main (String[] args)
+  {
+    int a=10, b=20;
+    if ( a<=b)
+    {
+     System.out.println(" a is greater than b"); 
+    }
+    if ( a>=b)
+    {
+      System.out.println(" b is smaller than a");
+    }
+  }
+}
