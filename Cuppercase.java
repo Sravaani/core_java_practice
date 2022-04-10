614e6d59f1704e5133ed58aed0bac3ee2ddb53c9
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-10 15:40:45 +0530

    Add files via upload

---
+++ Cuppercase.java
@@ -0,0 +1,24 @@
+//Write a Java program to check whether a character is uppercase or lowercase     alphabet.
+import java.util.Scanner;
+
+class Cuppercase
+  {
+    public static void main(String[] args){
+      char ch;
+     Scanner s=new Scanner(System.in);
+     System.out.println("Enter the character  ");
+       ch=s.next().charAt(0);
+      if(ch>='A' && ch<='Z')
+      {
+        System.out.println(ch+" is an upper case letter ");
+      }
+      else if(ch>='a' && ch<='z'){
+        System.out.println(ch+" is a lower case letter ");
+}
+else{
+    System.out.println(ch+" is not a Alphabets ");
+}
+}
+}
+    
+
