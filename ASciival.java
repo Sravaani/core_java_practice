9c0e1ed386d1fd1dfb120e8d6df7e0c9024c4c8e
Sravaani <102364220+Sravaani@users.noreply.github.com>
2022-04-11 18:26:19 +0530

    Add files via upload

---
+++ ASciival.java
@@ -0,0 +1,13 @@
+//Write a Java program to print all ASCII character with their values.
+
+class Asciival {
+
+    public static void main(String[] args)
+  {
+         for(int i = 0; i <= 255; i++)  
+           {  
+System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+}  
+} 
+}
+   
