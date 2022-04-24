ca80f15a5c1609e884f288922a654dfa3294af6e
Sravanigolla <102364220+Sravanigolla@users.noreply.github.com>
2022-04-24 13:18:02 +0530

    Add files via upload

---
+++ Assignments folder/GroceryItems (1).java
@@ -0,0 +1,51 @@
+/* JFM1T5_Assignment 2:
+
+   Write a program to store your 10 different expenses done in this month and print the highest price you incurred.
+   Prompt the user for the 10 numbers to be input from the terminal.
+   Sample Input:
+   45.90
+   23.56
+   123
+   23
+   90.00
+   344
+   44
+   60.01
+   267
+   100
+   
+   Expected Output:
+
+   The highest price you incurred is 344.0
+
+*/
+
+import java.util.Scanner;
+
+public class GroceryItems {
+public static void main(String args[]){
+//Define the main method
+
+//Declare and initialize variables
+    float size;
+   Scanner s=new Scanner(System.in);
+  System.out.println("Enter the size of an array:");
+//Take the 10 different expenses price
+  size=s.nextFloat(); // 5
+float max=0;
+float a[]=new float[10];
+System.out.println("Enter the values of array" +size);
+for (int i = 0; i < size; i++)
+{
+//Check the 10 different expenses price using for loop
+     a[i]=s.nextFloat();
+//Calculate the highest price in all expenses and check if the expenses is more than new item. If true then return max.
+if(a[i]>max)
+{ 
+max=a[i]; 
+}
+}
+//Print the result
+  System.out.println("Max value : " +max);
+}
+}
---
+++ Assignments folder/Temperature.java
@@ -0,0 +1,52 @@
+/* JFM1T5_Assignment3:
+
+   Write a program to store the temperature of your city for last 7 days and print the lowest temperature of the week.
+   Prompt the user input from the terminal.
+   
+   Sample Input:
+   Enter number of days 
+   3
+   Enter the temperature of Day :1
+   88
+   Enter the temperature of Day :2
+   34
+   Enter the temperature of Day :3
+   0 
+   
+   Expected Output:
+   The lowest temperature of the week 3 is 0.0 celsius
+
+*/
+
+
+import java.util.Scanner;
+
+public class Temperature{
+  public static void main(String[] args){
+//Define the main method
+//Declare the variables and initialize
+int min=0;
+    int i;
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter n value");
+    int size=s.nextInt();
+    int arr[]=new int[size];
+//Take temperature of 7 days as a input from user
+for(i=0;i<size;i++)
+{
+ System.out.println("Enter the temperature of day :"+(i+1));
+      arr[i]=s.nextInt();
+      }
+        min=arr[0];
+//Calculate the lowest temperature
+      for(i=0;i<size;i++)
+      {
+        if(arr[i]<min)
+        {
+         min=arr[i];
+          }
+        }
+System.out.println("Lowest temperature of the week 3 is: " +(float)min+" celsisu");  
+//Print the result
+}
+}
